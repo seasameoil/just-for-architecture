aae0157a7b7db657fb68ac6338a19889a1dd1e03
seasameoil <whdwlgus12@gmail.com>
2023-07-05 02:33:35 +0900

    comment build 관련수정

--- src/main/java/com/example/architecture/repository/CommentRepository.java
+++ src/main/java/com/example/architecture/repository/CommentRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByPost(Post post);
+    void deleteAllByPost(Optional<Post> byId);
 }
